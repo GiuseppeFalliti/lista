9b9180c5d19f23e4481ea3cb8f1b853d93f53419
GiuseppeFalliti <141495925+GiuseppeFalliti@users.noreply.github.com>
2023-11-16 19:50:49 +0100

    Update Mainapp.java

--- RegexDate/Mainapp.java
+++ RegexDate/Mainapp.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class Mainapp {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         ControlloDate data = new ControlloDate();
-        data.addData("12/04/2006");
+        System.out.println("inserire la data:");
+        data.addData(sc.nextLine());
     }
 }
