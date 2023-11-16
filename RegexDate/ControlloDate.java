503b6768ef5e865a5f8de21a826fe300d77771b6
GiuseppeFalliti <isfuari66@gmail.com>
2023-11-16 15:31:14 +0100

    update ControlloDate and create Mainapp

--- RegexDate/ControlloDate.java
+++ RegexDate/ControlloDate.java
@@ -1,50 +1,34 @@
-import java.util.regex.Pattern;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
- /**
- * @author Giuseppe Falliti & Riccardo Ponzano.
- * @version 1.0
-*/
-public class ControlloDate{
-    int years,month,day;
+
+public class ControlloDate {
     private LocalDate date;
     private final String regex = "^(0[1-9]|[1-9]|[12][0-9]|3[01])\\/(0[1-9]|[1-9]|1[0-2])\\/(\\d{4})$";
-    private final int[] DAYS ={31,29,31,30,31,30,31,30,30,31,30,31};
+    private final int[] DAYS = {31, 29, 31, 30, 31, 30, 31, 30, 30, 31, 30, 31};
 
     public void addData(String data) {
-        if(!data.matches(regex)){
-            System.out.println(false);
-        }
-        
-        date = LocalDate.parse(data,DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-        String[] splitDate = date.split("\\/");
-        this.years= Integer.parseInt(splitDate[2]);
-        this.month= Integer.parseInt(splitDate[1]);
-        this.day= Integer.parseInt(splitDate[0]);
-
-        if(this.day<=DAYS[this.month-1] ){
-            System.out.println("days existed");
+        if (!data.matches(regex)) {
+            System.out.println("Formato data non valido");
+            return;
         }
 
-        else{
-            System.out.println("days not existed");
+        date = LocalDate.parse(data, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        int day = date.getDayOfMonth();
+        int month = date.getMonthValue();
+        int year = date.getYear();
+
+        if (day <= DAYS[month - 1]) {
+            System.out.println("Giorno esistente");
+        } else {
+            System.out.println("Giorno non esistente");
         }
 
-        if(this.years%4==0 && this.years%400==0 && this.years%100!=0){
-            System.out.println("years bisestile");
+        if (year % 4 == 0 && (year % 400 == 0 || year % 100 != 0)) {
+            System.out.println("Anno bisestile");
+        } else {
+            System.out.println("Anno non bisestile");
         }
-        else{
-            System.out.println("years not bisestile");
-        }
-    
-        System.out.println("data valida:" + date);
-                
-    }
 
-    public static void main(String[] args) {
-        ControlloDate app = new ControlloDate();
-        app.addData("05/03/2023");
-    
+        System.out.println("Data valida: " + date);
     }
 }
