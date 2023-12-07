e98d1d663f522c9749632fde98fdafeddde7d17f
GiuseppeFalliti <141495925+GiuseppeFalliti@users.noreply.github.com>
2023-12-07 08:10:20 +0100

    Update Mainapp.java

--- RegexDate/Mainapp.java
+++ RegexDate/Mainapp.java
@@ -1,9 +1,38 @@
 import java.util.Scanner;
 public class Mainapp {
+
+    public static void addData(Scanner sc, ControlloDate data,String dataString){
+        data.addData(dataString);
+
+    }
+    public static void plus(Scanner sc, ControlloDate data,String dataString){
+        System.out.println("inserisci i giorni da aggiungere");
+        int giorni=sc.nextInt();
+        data.plus(dataString, giorni);
+        
+    }  
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ControlloDate data = new ControlloDate();
-        System.out.println("inserire la data nel seguente formato dd--MM-yyyy:");
-        data.addData(sc.nextLine());
+        System.out.println("inserisci la data");
+        String date=sc.nextLine();
+        System.out.println();
+        addData(sc, data,date);
+
+        System.out.println();
+        System.out.println("1.aggiungi giorni alla data");
+        System.out.println("2.exit");
+         String scelta=sc.nextLine();
+
+         while (true) {
+             switch (scelta) {
+            case "1":
+                plus(sc, data,date);
+                break;
+         }
+        
+
+       
     }
+}
 }
